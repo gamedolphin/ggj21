22c776224ff3487cfd11b1c253d9fb6c18f18b13
Sandeep Nambiar <sandeep.turing@gmail.com>
2021-01-30 16:13:33 +0100

    feat: fixed boid rotation!

--- Assets/Scripts/Sandeep/Boid.cs
+++ Assets/Scripts/Sandeep/Boid.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IBoid {
+public interface IBoid
+{
     public Vector2 UpdateBoid(List<Boid> allBoids);
 }
 
@@ -13,14 +14,16 @@ public class Boid : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 10;
 
-    public float MaxSpeed {
+    public float MaxSpeed
+    {
         get
         {
             return maxSpeed;
         }
     }
 
-    public Rigidbody2D RBody {
+    public Rigidbody2D RBody
+    {
         get
         {
             return rBody;
@@ -32,7 +35,7 @@ public class Boid : MonoBehaviour
     public float CohesionMultiplier = 1.0f;
     public float PathMultiplier = 4.0f;
 
-    public Rect WorldRect = new Rect(0,0,20,20);
+    public Rect WorldRect = new Rect(0, 0, 20, 20);
 
     private List<IBoid> otherBehaviours = new List<IBoid>();
     private System.Action<Boid> onDestroy;
@@ -60,7 +63,8 @@ public class Boid : MonoBehaviour
     public void UpdateBoid(List<Boid> allBoids)
     {
         var force = Vector2.zero;
-        foreach(var boid in otherBehaviours) {
+        foreach (var boid in otherBehaviours)
+        {
             force += boid.UpdateBoid(allBoids);
         }
         rBody.AddForce(force);
@@ -71,8 +75,8 @@ public class Boid : MonoBehaviour
     private void SteerToVelocity()
     {
         Vector2 v = rBody.velocity;
-        var angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg ;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        var angle = -Mathf.Atan2(v.x, v.y) * Mathf.Rad2Deg + Mathf.PI / 2;
+        rBody.SetRotation(Mathf.LerpAngle(rBody.rotation, angle, 0.1f));
     }
 
     private void OnDestroy()
