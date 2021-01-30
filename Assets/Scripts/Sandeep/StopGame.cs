c9121aa5442d83dca4dc7aec8a4491cea0aeb758
Sandeep Nambiar <sandeep.turing@gmail.com>
2021-01-30 18:59:16 +0100

    feat: level won

--- Assets/Scripts/Sandeep/StopGame.cs
+++ Assets/Scripts/Sandeep/StopGame.cs
@@ -5,12 +5,24 @@ using DG.Tweening;
 
 public class StopGame : MonoBehaviour
 {
+
+    public GameObject levelWon;
+
     private void Start()
     {
+        levelWon.SetActive(false);
         GameManager.Instance.onGameWin += OnGameWon;
     }
 
     private void OnGameWon(Vector3 pos)
     {
+        StartCoroutine(ShowGameWon());
+    }
+
+    private IEnumerator ShowGameWon()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Time.timeScale = 0;
+        levelWon.SetActive(true);
     }
 }
